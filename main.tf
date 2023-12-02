f664b21280badfbd8442d7c092a077ffa9850795
Swapnil-Dhurve <110110174+Swapnil-Dhurve@users.noreply.github.com>
2023-12-02 11:08:26 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,8 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = "${var.ami_id}"
     instance_type = "${var.instance_type}"
-  
-}
 
-tags = {
-    name= "HelloWorldTerraformCloud"
-    team= "DevOps"
+    tags = {
+     Name= "HelloWorldTerraformCloud"
+     Team= "DevOps"
 }
